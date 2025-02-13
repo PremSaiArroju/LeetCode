04e5ccbcf96a34e4226e362052b0b43ead4d6361
Prem Sai Arroju <premsai7373@gmail.com>
2025-02-12 22:39:51 -0600

    Time: 0 ms (100%), Space: 49.2 MB (21.01%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    // Ignore trailing spaces
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    // Count the length of the last word
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+};
