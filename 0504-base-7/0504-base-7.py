90fe37215de0c32be7b705dccfb342c878df38e5
Prem Sai Arroju <premsai7373@gmail.com>
2025-04-29 13:54:23 -0500

    Time: 0 ms (100%), Space: 17.9 MB (22.97%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        negative = num < 0
+        num = abs(num)
+        res = ""
+
+        while num > 0:
+            res = str(num%7) + res
+            num //= 7
+        
+        return "-" + res if negative else res
